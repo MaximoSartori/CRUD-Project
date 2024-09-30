d776db2a80f366db2f64b6eba5a0cf7163605d13
Maximo Sartori <maximosartori22@gmail.com>
2024-09-30 00:27:17 -0300

    Initial Commit

---
+++ Guia_Telefonica1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Guia_Telefonica1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(38, 61);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 20);
+            this.tb1.TabIndex = 0;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(38, 111);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 20);
+            this.tb2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NOMBRE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TELEFONO";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(188, 50);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(180, 96);
+            this.rtb1.TabIndex = 4;
+            this.rtb1.Text = "";
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(44, 200);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 5;
+            this.b1.Text = "CARGAR";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(165, 200);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 23);
+            this.b2.TabIndex = 6;
+            this.b2.Text = "MOSTRAR";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(280, 22);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 23);
+            this.b3.TabIndex = 7;
+            this.b3.Text = "LIMPIAR";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(199, 22);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(75, 23);
+            this.b4.TabIndex = 8;
+            this.b4.Text = "LISTAR";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(286, 200);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(75, 23);
+            this.b5.TabIndex = 9;
+            this.b5.Text = "ELIMINAR";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 277);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+    }
+}
+
---
+++ Guia_Telefonica1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_Telefonica1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<String, String> personas = new Dictionary<String, String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            String name = tb1.Text;
+            String num = tb2.Text;
+
+            personas.Add(name, num);
+            tb1.Clear();
+            tb2.Clear();
+
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            String llave = tb1.Text;
+            if (personas.ContainsKey(llave))
+            {
+                rtb1.Clear();
+                rtb1.Text =$"El nombre `{llave}`  tiene asociado el numero `{personas[llave]}`";
+            }
+            else {
+                rtb1.Clear();
+                rtb1.Text = "El nombre ingresado no existe";
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtb1.Clear();
+            rtb1.AppendText("NOMBRE     NUMERO\n");
+            foreach (var persona in personas)
+            {
+                
+                rtb1.AppendText($"   {persona.Key}      -      {persona.Value}\n");
+            }
+            
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            rtb1.Clear();
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            rtb1.Clear();
+            String nombr = tb1.Text;
+            if (personas.ContainsKey(nombr)){
+                personas.Remove(nombr);
+                rtb1.AppendText("La persona se elimino correctamente");
+            }
+            else{
+                rtb1.AppendText("Selecciona un nombre valido");
+            }
+                
+            
+        }
+    }
+}
